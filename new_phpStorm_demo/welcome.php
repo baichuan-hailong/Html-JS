6bb5227e1fd4871598b231e446f04a106ffe89ce
Du Hailong <1719812714@qq.com>
2017-09-15 19:50:01 +0800

    update

---
+++ new_phpStorm_demo/welcome.php
@@ -0,0 +1,23 @@
+<!--/**-->
+<!-- * Created by PhpStorm.-->
+<!-- * User: duhailong-->
+<!-- * Date: 2017/9/14-->
+<!-- * Time: 下午3:10-->
+<!-- */-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome Title</title>
+</head>
+<body>
+姓名 --- <?PHP echo $_POST["name"]; ?><br>
+电话 --- <?PHP echo $_POST["tel"]; ?><br>
+
+
+</body>
+</html>
+
+
