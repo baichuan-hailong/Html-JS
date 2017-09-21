e0cc17eb9ef667cd8d7371c504958e6fbf5e0717
Duhailong <1719812714@qq.com>
2017-09-21 08:59:55 +0800

    update

---
+++ welcome0/insert.php
@@ -0,0 +1,30 @@
+<?php
+    $servername = "localhost";
+    $username   = "bdm295719223";
+    $password   = "hailong66";
+    $dbname     = "bdm295719223_db";
+
+    $mes =  $_POST['message'];
+    $tel =  $_POST['tel'];
+
+    // 创建连接
+    $conn = new mysqli('bdm295719223.my3w.com', $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("连接失败: " . $conn->connect_error);
+    } 
+     
+
+    // 插入数据
+    $sql = "INSERT INTO messageBoard_table (message,tel)
+    VALUES ('$mes','$tel')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "新记录插入成功";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+    
+?> 
+
+
---
+++ welcome0/mysql.php
@@ -0,0 +1,30 @@
+<?php
+    $servername = "localhost";
+    $username   = "bdm295719223";
+    $password   = "hailong66";
+    $dbname     = "bdm295719223_db";
+
+    // 创建连接
+    $conn = new mysqli('bdm295719223.my3w.com', $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("连接失败: " . $conn->connect_error);
+    } 
+     
+    $sql = "SELECT id, message, last_update FROM messageBoard_table";
+    $result = $conn->query($sql);
+     
+
+
+    // 输出数据
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo $row["id"] . "、". $row["message"]."<br>";
+        }
+    } else {
+        echo "赶紧留言吧！";
+    }
+
+    $conn->close();
+    ?> 
+
+
